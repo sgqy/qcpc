41705a66c864c2bde8cb4a201a0b1d72e6635928
aki <wiki908.zh@gmail.com>
2015-11-06 19:16:31 +0800

    add code

---
+++ qcpc/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace qcpc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.Write(
+                    "use command like this:\n" +
+                    "echo <scp>[-dcp[-fstr]] | qcpc <input> [output]\n" +
+                    "example:\n" +
+                    "echo 932 - 1200 | qcpc in.txt out.txt\n" +
+                    "echo 932 - 936 - % | qcpc conv.txt"
+                    );
+                return;
+            }
+
+            //==============================================
+
+            string srcFile = null;
+            string dstFile = null;
+
+            try { srcFile = args[0]; } catch { }
+            try { dstFile = srcFile; } catch { }
+            try { dstFile = args[1]; } catch { }
+
+            if (srcFile == null) return;
+
+            string[] CPPair = Console.ReadLine().Split('-');
+
+            var srcCP = Encoding.GetEncoding(0);
+            var dstCP = Encoding.GetEncoding(1200);
+            var erfb = EncoderFallback.ReplacementFallback;
+            var drfb = DecoderFallback.ReplacementFallback;
+
+            try { srcCP = Encoding.GetEncoding(int.Parse(CPPair[0])); } catch { }
+            try { erfb = new EncoderReplacementFallback(CPPair[2].Trim()); } catch { }
+            try { dstCP = Encoding.GetEncoding(int.Parse(CPPair[1]), erfb, drfb); } catch { }
+            
+            FileStream fr = null;
+            try { fr = new FileStream(srcFile, FileMode.Open); } catch { Console.WriteLine("import file error"); return; }
+            var ibuf = new byte[fr.Length];
+            fr.Read(ibuf, 0, ibuf.Length);
+            fr.Dispose();
+
+            var obuf = Encoding.Convert(srcCP, dstCP, ibuf);
+            
+            FileStream fw = null;
+            try { fw = new FileStream(dstFile, FileMode.OpenOrCreate); } catch { Console.WriteLine("export file error"); return; }
+            fw.SetLength(0);
+            fw.Write(obuf, 0, obuf.Length);
+            fw.Dispose();
+        }
+    }
+}
